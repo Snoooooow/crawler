6ec423589d5265c231e2bd25023c7f57d97fad4d
Jing YU <yujinglove1992@gmail.com>
2018-05-13 17:11:15 -0700

    Sample

---
+++ crawler.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found; %s %q\n", url, body)
+	for _, u := range urls{
+		Crawl(u, depth, fetcher)
+	}
+	return
+}
+
+func (f fakeFetcher) Fetch(url string)(string, []string, error){
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+func main(){
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
